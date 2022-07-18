352379904c84e803cef8269cc5d9d3bd0c45cd98
Adonai Dominguez Hernandez <donihernandez003@gmail.com>
2022-07-18 17:00:43 -0400

    project finished

--- app/src/main/java/com/doni/myquizapp/Constants.kt
+++ app/src/main/java/com/doni/myquizapp/Constants.kt
@@ -2,7 +2,11 @@ package com.doni.myquizapp
 
 object Constants {
 
-    fun getQuestions(): ArrayList<Question> {
+    const val USER_NAME: String = "user_name"
+    const val TOTAL_QUESTIONS: String = "total_questions"
+    const val CORRECT_ANSWERS: String = "correct_answers"
+
+    fun getQuestions(): ArrayList<Question>  {
        val questionsList = ArrayList<Question>()
 
         val que1 = Question(
